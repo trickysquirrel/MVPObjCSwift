615c43d0041766fc112beb05d4a1880a93241e84
Richard Moult <richard.moult@iflix.com>
2020-01-04 13:55:01 +1100

    Initial Commit

---
+++ MVPObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MVPObjC
+//
+//  Created by Richard Moult on 4/1/20.
+//  Copyright © 2020 RichardMoult. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
